039985a548bf184501efffaa0a46aebedf6da625
Ahleeryan <ahleeryan@hotmail.com>
2026-09-17 10:49:44 -0500

    working clerk middleware

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,26 @@
 import express, { type Express, type Request, type Response } from 'express';
 
+import { clerkMiddleware } from '@clerk/express';
+// clerk middleware
+
+import 'dotenv/config';
+// remidner: dotenv is needed now to handle loading env vairables. prisma no longer automatically loads .env stuff here?
+
+
 const app: Express = express();
 const port = 3000;
+
+
+
+app.use(clerkMiddleware());
+
+
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
