02746bbf8a926b70f37c88db19df1a5f5f8fd14c
Ahleeryan <ahleeryan@hotmail.com>
2026-09-22 11:42:17 -0500

    ingredients and instructions functional list.

--- client/src/pages/createRecipe.tsx
+++ client/src/pages/createRecipe.tsx
@@ -6,7 +6,14 @@ export default function CreateRecipe() {
     
     const [directionInput, setDirectionInput] = useState('');
     const [directions, setDirections] = useState([]);
-    
+
+    const [ingredientInput, setIngredientInput] = useState('');
+    const [ingredients, setIngredients] = useState([]);
+
+    const [categoryList, setCategoryList] = useState ([]); // for database
+    const [category, setCategory] = useState(''); // for chosen
+    const [selectedCategoryList, setSelectedCategoryList] = useState ([]); // for containing selected.
+
 
     // adding direction to the form.
     const handleDirectionsSubmit = (e) => {
@@ -39,12 +46,39 @@ export default function CreateRecipe() {
 
     //add ingredients
 
+    const handleIngredientSubmit = (e) => {
+         e.preventDefault();
+         if(!ingredientInput.trim()) return;
+        console.log("inputting ingredient! here is your ingredient: ", ingredientInput);
+        
+        setIngredients((prev) => [...prev, ingredientInput.trim()]);
 
+        setIngredientInput('');
+        console.log("The updated list: ", ingredients);
+
+    }
 
     // remove ingredients
-    // delete directions
+    const handleDeleteIngredient = (e) => {
+        console.log("called for deletion: ", e);
+
+        // in the directions array. filder, reference order, delete order. 
+        setIngredients(prev => prev.filter((_, i) => i != e));
+    }
     
 
+
+
+
+    // get from database 
+    // need to fill the container. does this even need to be a usestate? it's not changing.
+    // maybe we can just defined it outside. as its own const.
+    // grab categories from database.
+    // if already applied, return.
+
+    // apply it to a use state array object. 
+
+    // where i last ended off.
     
     
 
@@ -59,10 +93,6 @@ export default function CreateRecipe() {
                         justifyContent: 'center',
                     }}>
 
-
-
-
-                        {/* we're going to focus on the directions first, then copy the pattern to the others. */}
 
                         <form style={{
                             margin: '0px 100px',
@@ -81,17 +111,17 @@ export default function CreateRecipe() {
 
 
 
-
-
-
                         <form style={{
                             margin: '0px 100px',
                             background: 'gray',
                         }}
+                        onSubmit={handleIngredientSubmit}
                         >
                             <label>ingredients</label>
                             <input
                                 placeholder="Enter your Recipe" 
+                                value={ingredientInput}
+                                onChange={(e) => setIngredientInput(e.target.value)}
                             ></input>
                             <button type="submit">Submit</button>
                         </form>
@@ -104,6 +134,14 @@ export default function CreateRecipe() {
 
                         <form> 
                             <label>Categories</label>
+
+                            <select>
+                                <option>one</option>
+                                <option>one</option>
+                                <option>one</option>
+                                <option>one</option>
+
+                            </select>
                             {/* dropdown titled categories with react references to change and manipulate categories. */}
                             <button type="submit">Submit</button>
                         </form>
@@ -139,14 +177,26 @@ export default function CreateRecipe() {
 
 
 
-
-
+                        
+                        {/* ingredients container */}
                         <ul style={{
                             margin: '0px 100px',
                             background: 'gray',
                         }}>
-                            <li>ingredient lists?</li>
+                            <label>ingredient lists?</label>
+                            {ingredients.map((item, i) => (
+                                <li key={i}>
+                                    {item}
+                                    <button type="button" onClick={() => handleDeleteIngredient(i)} >✕</button> 
+                                    <p>Index: {i}</p>
+                                </li>
+                            ))
+                            }
                         </ul>
+
+
+
+
                         <ul style={{
                             margin: '0px 100px',
                             background: 'gray',
