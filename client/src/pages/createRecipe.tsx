506331d74f7e19c531cf70e4d4bd38afc2b44955
Ahleeryan <ahleeryan@hotmail.com>
2026-09-22 11:09:48 -0500

    working directions list addition

--- client/src/pages/createRecipe.tsx
+++ client/src/pages/createRecipe.tsx
@@ -1,10 +1,50 @@
 import { SignedIn, SignedOut } from "@clerk/clerk-react"
-import { useState } from "react"
+import { use, useState } from "react"
 
 export default function CreateRecipe() {
 
-
     
+    const [directionInput, setDirectionInput] = useState('');
+    const [directions, setDirections] = useState([]);
+    
+
+    // adding direction to the form.
+    const handleDirectionsSubmit = (e) => {
+        e.preventDefault();
+        if(!directionInput.trim()) return;
+        console.log("direction submit activated: ", directionInput);
+
+        setDirections((prev) => [...prev, directionInput.trim()]);
+
+        
+        setDirectionInput('');
+
+        console.log("The updated list: ", directions);
+    }
+
+
+
+    // delete directions
+    const handleDeleteDirection = (e) => {
+        console.log("called for deletion: ", e);
+
+        // in the directions array. filder, reference order, delete order. 
+        setDirections(prev => prev.filter((_, i) => i != e));
+    }
+
+
+
+
+
+
+    //add ingredients
+
+
+
+    // remove ingredients
+    // delete directions
+    
+
     
     
 
@@ -19,22 +59,48 @@ export default function CreateRecipe() {
                         justifyContent: 'center',
                     }}>
 
+
+
+
+                        {/* we're going to focus on the directions first, then copy the pattern to the others. */}
+
                         <form style={{
                             margin: '0px 100px',
                             background: 'gray',
-                        }}>
+                        }}
+                        onSubmit={handleDirectionsSubmit} 
+                        >
                             <label>directions</label>
-                            <input></input>
+                            <input placeholder="Enter your Directions"
+                                value={directionInput}
+                                onChange={(e) => setDirectionInput(e.target.value)}
+                            ></input>
                             <button type="submit">Submit</button>
                         </form>
+
+
+
+
+
+
+
                         <form style={{
                             margin: '0px 100px',
                             background: 'gray',
-                        }}>
+                        }}
+                        >
                             <label>ingredients</label>
-                            <input></input>
+                            <input
+                                placeholder="Enter your Recipe" 
+                            ></input>
                             <button type="submit">Submit</button>
                         </form>
+
+
+
+
+
+
 
                         <form> 
                             <label>Categories</label>
@@ -47,12 +113,34 @@ export default function CreateRecipe() {
                         display: 'flex', 
                         justifyContent: 'center',
                     }}>
+
+
+
+                        {/* directions listing container */}
                         <ul style={{
                             margin: '0px 100px',
                             background: 'gray',
                         }}>
-                            <li>directions item?</li>
+                            <label>directions item?</label>
+                            {directions.map((item, i) => (
+                            
+                                <li key={i}> 
+                                    {item}
+
+                                    <button type="button" onClick={() => handleDeleteDirection(i)} >✕</button> 
+                                    {/*  */} 
+                                    <p>Index: {i}</p>
+                                </li>
+                                    
+                            ))}
+                            
                         </ul>
+
+
+
+
+
+
                         <ul style={{
                             margin: '0px 100px',
                             background: 'gray',
